264150253cd8be043a8bb14ab726043ebb59222e
DelmMV <opi588@yandex.ru>
2022-03-07 16:04:16 +0300

    fix: Поправить замечания ревьюра

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,47 @@
 // popup open/close
 let popup = document.querySelector('.popup');
-let popupClose = popup.querySelector ('.popup__btn_close');
+let popupCloseBtn = popup.querySelector ('.popup_btn_close');
 let profile = document.querySelector ('.profile');
-let profileEditBtn = profile.querySelector('.profile__btn_edit')
+let profileEditBtn = profile.querySelector('.profile_btn_edit');
 
-let nameP = profile.querySelector('.profile__title');
-let whoP = profile.querySelector('.profile__subtitle');
+let profileTitle = profile.querySelector('.profile__title');
+let profileSubtitle = profile.querySelector('.profile__subtitle');
 
 let formElement = document.querySelector('.popup__form');
-let nameInput = popup.querySelector('#name');
-let nameWho = popup.querySelector('#who');
+let PopupInputName = popup.querySelector('#name');
+let PopupInputWho = popup.querySelector('#who');
 
 // Like active button
-let likeBtn = document.querySelectorAll('.cards__like-btn');
-likeBtn.forEach(function(like) {
+let cardsLikeBtn = document.querySelectorAll('.cards__like-btn');
+cardsLikeBtn.forEach(function(like) {
   like.addEventListener('click', function() {
-    like.classList.toggle('cards__like-btn_active')
+    like.classList.toggle('cards_like-btn_active')
   });
 });
 
 // Close popup
-function closeP() {
-  popup.classList.remove('popup_open');
-
-  nameInput.value = nameP.textContent;
-  nameWho.value = whoP.textContent;
-
+function closePopup() {
+  popup.classList.remove('popup_opened');
 }
-popupClose.addEventListener('click', closeP)
+popupCloseBtn.addEventListener('click', closePopup)
 
 // Edit popup
-function editP() {
-  popup.classList.add('popup_open')
+function editPopup() {
+  popup.classList.add('popup_opened')
 
-  nameInput.value = nameP.textContent;
-  nameWho.value = whoP.textContent;
+  PopupInputName.value = profileTitle.textContent;
+  PopupInputWho.value = profileSubtitle.textContent;
 }
-profileEditBtn.addEventListener('click', editP)
+profileEditBtn.addEventListener('click', editPopup)
 
 // Save Popup Profile
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  nameP.textContent = nameInput.value;
-  whoP.textContent = nameWho.value;
+  profileTitle.textContent = PopupInputName.value;
+  profileSubtitle.textContent = PopupInputWho.value;
 
-  closeP()
+  closePopup()
 };
 
 formElement.addEventListener('submit', formSubmitHandler);
