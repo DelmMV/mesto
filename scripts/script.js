0d8ebd20cdb9538f379c387804b4d93c7ce82066
DelmMV <96190607+DelmMV@users.noreply.github.com>
2022-03-15 21:29:43 +0300

    feat: Добавить лайки

--- scripts/script.js
+++ scripts/script.js
@@ -39,23 +39,28 @@ const popupTypeAdd = document.querySelector(".popup_type_add");
 const popupTypeEdit = document.querySelector(".popup_type_edit");
 const popupInputName = popupEdit.querySelector("#name");
 const popupInputWho = popupEdit.querySelector("#who");
-const popupInputNames = popupAdd.querySelector("#names")
-const popupInputImage = popupAdd.querySelector("#image-url")
+const popupInputNames = popupAdd.querySelector("#names");
+const popupInputImage = popupAdd.querySelector("#image-url");
 const formElementEdit = popupTypeEdit.querySelector(".popup_form_edit");
 const formElementAdd = popupTypeAdd.querySelector(".popup_form_add");
-const cardsList = document.querySelector(".cards__list")
+const cardsList = document.querySelector(".cards__list");
+
+
+
 
 
 //rendering cards
 function renderCards(cardsContent) {
   const cards = document.querySelector(".cards-template").content.firstElementChild.cloneNode(true);
-  const cardsImage = cards.querySelector(".cards__image")
+  const cardsImage = cards.querySelector(".cards__image");
+  const cardsLikeBtn = (event) => event.target.classList.toggle("cards__like-btn_active");
 
   cards.querySelector(".cards__title").textContent = cardsContent.name;
   cardsImage.src = cardsContent.link;
   cardsImage.alt = cardsContent.name;
   setCardsActionListeners(cards);
-  cardsList.append(cards);
+  cards.querySelector(".cards__like-btn").addEventListener("click", cardsLikeBtn);
+  cardsList.prepend(cards);
 }
 
 function removeCards(event) {
@@ -66,6 +71,9 @@ function setCardsActionListeners(cards) {
   cards.querySelector(".cards__delete-btn").addEventListener("click", removeCards);
 }
 
+function addPopup() {
+  popupAdd.classList.add("popup_opened");
+}
 
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
@@ -75,10 +83,6 @@ function editPopup() {
   popupEdit.classList.add("popup_opened");
   popupInputName.value = profileTitle.textContent;
   popupInputWho.value = profileSubtitle.textContent;
-}
-
-function addPopup() {
-  popupAdd.classList.add("popup_opened");
 }
 
 function formSubmitHandlerEdit(evt) {
